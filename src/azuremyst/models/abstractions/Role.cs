a32ad8da96ed5855705f032a2e0febdfa400e930
confessore <confessore@dev0.lan>
2022-05-14 23:44:17 -0400

    usings

--- src/azuremyst/models/abstractions/Role.cs
+++ src/azuremyst/models/abstractions/Role.cs
@@ -1,4 +1,10 @@
-﻿namespace azuremyst.models.abstractions
+﻿using azuremyst.converters;
+using azuremyst.models.enums;
+using azuremyst.models.interfaces;
+using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
+
+namespace azuremyst.models.abstractions
 {
     public abstract class Role : IdentityRole, IEntity<string>, IRole
     {
