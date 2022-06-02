620c9fccb63e9828d944ec49cd4b1987d5c7f6c3
steven confessore <confessore@balasolu.com>
2022-06-01 22:18:58 -0400

    working on boost

--- src/azuremyst/services/BoostService.cs
+++ src/azuremyst/services/BoostService.cs
@@ -18,10 +18,106 @@ namespace azuremyst.services
             _soapService = soapService;
         }
 
+        public async Task<bool> Boost60Async(string name)
+        {
+            using var context = await _characterDbContextFactory.CreateDbContextAsync();
+            if (context.Characters != null)
+            {
+                var character = await context.Characters.FirstOrDefaultAsync(x => x.Name.ToLower() == name.ToLower());
+                if (character != null)
+                {
+                    if (character.Level < 60)
+                    {
+                        await _soapService.CharacterLevel(name, 60);
+                        // modify money for spells (only like 150g)
+                        // teach 150/150 mount
+                    }
+                    switch ((WoWClass)character.Class)
+                    {
+                        case WoWClass.Druid:
+                            {
+                                await _soapService.SendItemsAsync(name, druid);
+                                await _soapService.SendItemsAsync(name, sharedCaster);
+                                await _soapService.SendItemsAsync(name, sharedHealer);
+                                await _soapService.SendItemsAsync(name, sharedPhysical);
+                                return true;
+                            }
+                        case WoWClass.Hunter:
+                            {
+                                await _soapService.SendItemsAsync(name, hunter);
+                                await _soapService.SendItemsAsync(name, sharedPhysical);
+                                return true;
+                            }
+                        case WoWClass.Mage:
+                            {
+                                await _soapService.SendItemsAsync(name, mage);
+                                await _soapService.SendItemsAsync(name, sharedCaster);
+                                return true;
+                            }
+                        case WoWClass.Paladin:
+                            {
+                                await _soapService.SendItemsAsync(name, paladin);
+                                await _soapService.SendItemsAsync(name, sharedCaster);
+                                await _soapService.SendItemsAsync(name, sharedHealer);
+                                await _soapService.SendItemsAsync(name, sharedPhysical);
+                                return true;
+                            }
+                        case WoWClass.Priest:
+                            {
+                                await _soapService.SendItemsAsync(name, priest);
+                                await _soapService.SendItemsAsync(name, sharedCaster);
+                                await _soapService.SendItemsAsync(name, sharedHealer);
+                                return true;
+                            }
+                        case WoWClass.Rogue:
+                            {
+                                await _soapService.SendItemsAsync(name, warrior);
+                                await _soapService.SendItemsAsync(name, sharedPhysical);
+                                return true;
+                            }
+                        case WoWClass.Shaman:
+                            {
+                                await _soapService.SendItemsAsync(name, shaman);
+                                await _soapService.SendItemsAsync(name, sharedCaster);
+                                await _soapService.SendItemsAsync(name, sharedHealer);
+                                await _soapService.SendItemsAsync(name, sharedPhysical);
+                                return true;
+                            }
+                        case WoWClass.Warlock:
+                            {
+                                await _soapService.SendItemsAsync(name, warlock);
+                                await _soapService.SendItemsAsync(name, sharedCaster);
+                                return true;
+                            }
+                        case WoWClass.Warrior:
+                            {
+                                await _soapService.SendItemsAsync(name, warrior);
+                                await _soapService.SendItemsAsync(name, sharedPhysical);
+                                return true;
+                            }
+                        default:
+                            return false;
+                    }
+                }
+                return false;
+            }
+            return false;
+        }
+
+        public static readonly int[] druid =
+        {
+
+        };
+
+        public static readonly int[] hunter =
+        {
+
+        };
+
         public static readonly int[] mage =
         {
             22062,
-            22063, 
+            22063,
             22064,
             22065,
             22066,
@@ -30,8 +126,28 @@ namespace azuremyst.services
             22069
         };
 
+        public static readonly int[] paladin =
+        {
+
+        };
+
+        public static readonly int[] priest =
+        {
+
+        };
+
+        public static readonly int[] rogue =
+        {
+
+        };
+
+        public static readonly int[] shaman =
+        {
+
+        };
+
         public static readonly int[] warlock =
-{
+        {
             22070,
             22071,
             22072,
@@ -40,6 +156,11 @@ namespace azuremyst.services
             22075,
             22076,
             22077
+        };
+
+        public static readonly int[] warrior =
+        {
+
         };
 
         public static readonly int[] sharedCaster =
@@ -54,38 +175,14 @@ namespace azuremyst.services
             22268
         };
 
-        public async Task<bool> Boost60Async(string name)
+        public static readonly int[] sharedHealer =
         {
-            using var context = await _characterDbContextFactory.CreateDbContextAsync();
-            if (context.Characters != null)
-            {
-                var character = await context.Characters.FirstOrDefaultAsync(x => x.Name.ToLower() == name.ToLower());
-                if (character != null)
-                {
-                    if (character.Level < 60)
-                        await _soapService.CharacterLevel(name, 60);
-                    switch ((WoWClass)character.Class)
-                    {
-                        // add support for the other classes
-                        case WoWClass.Mage:
-                        {
-                            await _soapService.SendItemsAsync(name, mage);
-                            await _soapService.SendItemsAsync(name, sharedCaster);
-                            return true;
-                        }
-                        case WoWClass.Warlock:
-                            {
-                                await _soapService.SendItemsAsync(name, warlock);
-                                await _soapService.SendItemsAsync(name, sharedCaster);
-                                return true;
-                            }
-                        default:
-                            return false;
-                    }
-                }
-                return false;
-            }
-            return false;
-        }
+
+        };
+
+        public static readonly int[] sharedPhysical =
+        {
+
+        };
     }
 }
